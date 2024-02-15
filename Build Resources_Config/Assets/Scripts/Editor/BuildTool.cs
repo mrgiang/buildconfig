735b838ba36150b4eb221833411379540de61162
mrllgiang <giangquocnhan2014@gmail.com>
2024-02-15 09:46:36 +0700

    first

---
+++ Build Resources_Config/Assets/Scripts/Editor/BuildTool.cs
@@ -0,0 +1,253 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+public class BuildTool : MonoBehaviour
+{
+	/// <summary>
+    /// Encrypt asset bundle file
+    /// </summary>
+    /// <param name="assetBundleFileDir"></param>
+    private static bool EncryptAssetBundleFile(string assetBundleFileDir)
+    {
+        if (!File.Exists(assetBundleFileDir))
+        {
+            Debug.LogError("Config.unity3d does not exist!!!");
+            return false;
+        }
+        KTResourceCrypto.SetKey("eabb22bdc77d8d8fc85f52a572ae2f52eabb22bdc77d8d8fc85f52a572ae2f52eabb22bdc77d8d8fc85f52a572ae2f52eabb22bdc77d8d8fc85f52a572ae2f52eabb22bdc77d8d8fc85f52a572ae2f52eabb22bdc77d8d8fc85f52a572ae2f52eabb22bdc77d8d8fc85f52a572ae2f52eabb22bdc77d8d8fc85f52a572ae2f52");
+        byte[] byteData = File.ReadAllBytes(assetBundleFileDir);
+
+        byte[] encryptedBytes = KTResourceCrypto.Encrypt(byteData.ToArray());
+
+        File.Delete(assetBundleFileDir);
+        File.WriteAllBytes(assetBundleFileDir, encryptedBytes);
+
+        return true;
+    }
+
+    /// <summary>
+    /// Build
+    /// </summary>
+    [MenuItem("Build Config/Android")]
+    public static void AndroidBuildConfig()
+    {
+        // Bring up save panel
+        var path = EditorUtility.SaveFilePanel("Save Resource", "", "Config", "unity3d");
+        if (path.Length != 0)
+        {
+            // Build the resource file from the active selection.
+            var selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+            BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path, BuildAssetBundleOptions.CompleteAssets, BuildTarget.Android);
+        }
+        bool ret = BuildTool.EncryptAssetBundleFile(path);
+        if (ret)
+        {
+            Debug.Log("Build Config.unity3d successfully!");
+        }
+        else
+        {
+            Debug.Log("Build Config.unity3d failed!");
+        }
+    }
+	    [MenuItem("Build Config/Ios")]
+    public static void IosBuildConfig()
+    {
+        // Bring up save panel
+        var path = EditorUtility.SaveFilePanel("Save Resource", "", "Config", "unity3d");
+        if (path.Length != 0)
+        {
+            // Build the resource file from the active selection.
+            var selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+            BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path, BuildAssetBundleOptions.CompleteAssets, BuildTarget.iOS);
+        }
+        bool ret = BuildTool.EncryptAssetBundleFile(path);
+        if (ret)
+        {
+            Debug.Log("Build Config.unity3d successfully!");
+        }
+        else
+        {
+            Debug.Log("Build Config.unity3d failed!");
+        }
+    }
+	
+	
+	
+	
+	
+	
+    /// <summary>
+    /// Adds newly (if not already in the list) found assets.
+    /// Returns how many found (not how many added)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path"></param>
+    /// <param name="assetsFound">Adds to this list if it is not already there</param>
+    /// <returns></returns>
+    public static T[] GetAssetsFromPath<T>(string path) where T : Object
+    {
+        string[] filePaths = Directory.GetFiles(path);
+
+        List<T> list = new List<T>();
+
+        if (filePaths != null && filePaths.Length > 0)
+        {
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                Object obj = AssetDatabase.LoadAssetAtPath(filePaths[i], typeof(T));
+                if (obj is T asset)
+                {
+                    if (!list.Contains(asset))
+                    {
+                        list.Add(asset);
+                    }
+                }
+            }
+        }
+
+        return list.ToArray();
+    }
+
+    [MenuItem("Build Res/Android - Build AssetBundle from selected")]
+    private static void AndroidBuildResource()
+    {
+        // Bring up save panel
+        var path = EditorUtility.SaveFilePanel("Save Resource", "", "New Resource", "unity3d");
+        if (path.Length != 0)
+        {
+            // Build the resource file from the active selection.
+            var selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+            BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path, BuildAssetBundleOptions.CompleteAssets, BuildTarget.Android);
+
+        }
+    }
+
+    [MenuItem("Build Res/Android - Build All AssetBundles inside selected folders")]
+    private static void AndroidBuildAllResources()
+    {
+        string outputFolder = EditorUtility.SaveFolderPanel("Save Resources", "Build - Android", "");
+        if (!string.IsNullOrEmpty(outputFolder))
+        {
+            Object[] selectedAssets = Selection.objects;
+            foreach (Object asset in selectedAssets)
+            {
+                Object[] subAssets = BuildTool.GetAssetsFromPath<Object>(AssetDatabase.GetAssetPath(asset));
+                BuildPipeline.BuildAssetBundle(asset, subAssets, outputFolder + "/" + asset.name + ".unity3d", BuildAssetBundleOptions.CompleteAssets, BuildTarget.Android);
+            }
+        }
+    }
+
+    [MenuItem("Build Res/Android - Build All Assets inside selected folders individually (For build MAP)")]
+    private static void AndroidBuildAllResourcesInsideFolderIndividually()
+    {
+        string outputFolder = EditorUtility.SaveFolderPanel("Save Resources", "Build - Android", "");
+        if (!string.IsNullOrEmpty(outputFolder))
+        {
+            Object[] selectedAssets = Selection.objects;
+            foreach (Object asset in selectedAssets)
+            {
+                Object[] subAssets = BuildTool.GetAssetsFromPath<Object>(AssetDatabase.GetAssetPath(asset));
+                foreach (Object subAsset in subAssets)
+                {
+                    BuildPipeline.BuildAssetBundle(subAsset, null, outputFolder + "/" + subAsset.name + ".unity3d", BuildAssetBundleOptions.CompleteAssets, BuildTarget.Android);
+                }
+            }
+        }
+    }
+
+    [MenuItem("Build Res/IOS - Build AssetBundle from selected")]
+    private static void IOSBuildResource()
+    {
+        // Bring up save panel
+        var path = EditorUtility.SaveFilePanel("Save Resource", "", "New Resource", "unity3d");
+        if (path.Length != 0)
+        {
+            // Build the resource file from the active selection.
+            var selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+            BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path, BuildAssetBundleOptions.CompleteAssets, BuildTarget.iOS);
+        }
+    }
+
+    [MenuItem("Build Res/IOS - Build All AssetBundles inside selected folders")]
+    private static void IOSBuildAllResources()
+    {
+        string outputFolder = EditorUtility.SaveFolderPanel("Save Resources", "Build - Android", "");
+        if (!string.IsNullOrEmpty(outputFolder))
+        {
+            Object[] selectedAssets = Selection.objects;
+            foreach (Object asset in selectedAssets)
+            {
+                Object[] subAssets = BuildTool.GetAssetsFromPath<Object>(AssetDatabase.GetAssetPath(asset));
+                BuildPipeline.BuildAssetBundle(asset, subAssets, outputFolder + "/" + asset.name + ".unity3d", BuildAssetBundleOptions.CompleteAssets, BuildTarget.iOS);
+            }
+        }
+    }
+
+    [MenuItem("Build Res/IOS - Build All Assets inside selected folders individually (For build MAP)")]
+    private static void IOSBuildAllResourcesInsideFolderIndividually()
+    {
+        string outputFolder = EditorUtility.SaveFolderPanel("Save Resources", "Build - Android", "");
+        if (!string.IsNullOrEmpty(outputFolder))
+        {
+            Object[] selectedAssets = Selection.objects;
+            foreach (Object asset in selectedAssets)
+            {
+                Object[] subAssets = BuildTool.GetAssetsFromPath<Object>(AssetDatabase.GetAssetPath(asset));
+                foreach (Object subAsset in subAssets)
+                {
+                    BuildPipeline.BuildAssetBundle(subAsset, null, outputFolder + "/" + subAsset.name + ".unity3d", BuildAssetBundleOptions.CompleteAssets, BuildTarget.iOS);
+                }
+            }
+        }
+    }
+
+    [MenuItem("Build Res/Windows - Build AssetBundle from selected")]
+    [System.Obsolete]
+    private static void WindowsBuildResource()
+    {
+        // Bring up save panel
+        var path = EditorUtility.SaveFilePanel("Save Resource", "", "New Resource", "unity3d");
+        if (path.Length != 0)
+        {
+            // Build the resource file from the active selection.
+            var selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+            BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path, BuildAssetBundleOptions.CompleteAssets, BuildTarget.StandaloneWindows64);
+        }
+    }
+
+    [MenuItem("Build Res/Windows - Build All AssetBundles inside selected folders")]
+    private static void WindowsBuildAllResources()
+    {
+        string outputFolder = EditorUtility.SaveFolderPanel("Save Resources", "Build - Android", "");
+        if (!string.IsNullOrEmpty(outputFolder))
+        {
+            Object[] selectedAssets = Selection.objects;
+            foreach (Object asset in selectedAssets)
+            {
+                Object[] subAssets = BuildTool.GetAssetsFromPath<Object>(AssetDatabase.GetAssetPath(asset));
+                BuildPipeline.BuildAssetBundle(asset, subAssets, outputFolder + "/" + asset.name + ".unity3d", BuildAssetBundleOptions.CompleteAssets, BuildTarget.StandaloneWindows64);
+            }
+        }
+    }
+
+    [MenuItem("Build Res/Windows - Build All Assets inside selected folders individually (For build MAP)")]
+    private static void WindowsBuildAllResourcesInsideFolderIndividually()
+    {
+        string outputFolder = EditorUtility.SaveFolderPanel("Save Resources", "Build - Android", "");
+        if (!string.IsNullOrEmpty(outputFolder))
+        {
+            Object[] selectedAssets = Selection.objects;
+            foreach (Object asset in selectedAssets)
+            {
+                Object[] subAssets = BuildTool.GetAssetsFromPath<Object>(AssetDatabase.GetAssetPath(asset));
+                foreach (Object subAsset in subAssets)
+                {
+                    BuildPipeline.BuildAssetBundle(subAsset, null, outputFolder + "/" + subAsset.name + ".unity3d", BuildAssetBundleOptions.CompleteAssets, BuildTarget.StandaloneWindows64);
+                }
+            }
+        }
+    }
+}
---
+++ Build Resources_Config/Assets/Scripts/Scale.cs
@@ -0,0 +1,24 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    [SerializeField] private Renderer Backgound ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator Scalebackgound()
+    {
+          
+        yield return null;
+    }
+}
